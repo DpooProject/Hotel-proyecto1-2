195a61c472b22104451df34e8aceab389f51d03b
santinajar <s.najar@uniandes.edu.co>
2023-05-03 15:55:47 -0500

    general Employer ya terminadp
    ya esta el general employer

--- Hotel/src/model/Bills.java
+++ Hotel/src/model/Bills.java
@@ -1,5 +1,59 @@
 package model;
 
+import java.util.HashMap;
+
 public class Bills {
+    private HashMap<String, Integer> roomBills;
+
+    public Bills() {
+        roomBills = new HashMap<String, Integer>();
+    }
+
+    public void addRoomBill(String roomId, int billAmount) {
+        roomBills.put(roomId, billAmount);
+    }
+
+    public int getRoomBill(String roomId) {
+        return roomBills.get(roomId);
+    }
+
+    public void removeRoomBill(String roomId) {
+        roomBills.remove(roomId);
+    }
+
+    public void clearBills() {
+        roomBills.clear();
+    }
+
+    public HashMap<String, Integer> getAllBills() {
+        return roomBills;
+    }
+    
+    public boolean addAmountToRoomBill(String roomId, int amountToAdd) {
+        if (!roomBills.containsKey(roomId)) {
+            return false; // la habitación no existe en el mapa, retornar false
+        }
+        int currentAmount = roomBills.get(roomId); // obtener el valor actual de la habitación
+        int newAmount = currentAmount + amountToAdd; // sumar la cantidad deseada
+        roomBills.put(roomId, newAmount); // actualizar el valor en el mapa
+        return true; // operación exitosa, retornar true
+    }
+    
+    public int getRoomBillAmount(String roomId) {
+        if (!roomBills.containsKey(roomId)) {
+            return -1; // la habitación no existe en el mapa, retornar -1
+        }
+        return roomBills.get(roomId); // retornar el valor actual de la habitación
+    }
+    public boolean payAmountToRoomBill(String roomId, int amountToAdd) {
+        if (!roomBills.containsKey(roomId)) {
+            return false; // la habitación no existe en el mapa, retornar false
+        }
+        int currentAmount = roomBills.get(roomId); // obtener el valor actual de la habitación
+        int newAmount = currentAmount - amountToAdd; // sumar la cantidad deseada
+        roomBills.put(roomId, newAmount); // actualizar el valor en el mapa
+        return true; // operación exitosa, retornar true
+    }
+
 
 }
--- Hotel/src/prosecution/EmployerProcess.java
+++ Hotel/src/prosecution/EmployerProcess.java
@@ -1,5 +1,28 @@
 package prosecution;
 
+import model.Bills;
 public class EmployerProcess {
+    private Bills bills;
+
+    public EmployerProcess() {
+        bills = new Bills();
+    }
+
+    public boolean addAmountToRoomBill(String roomId, int amountToAdd) {
+        return bills.addAmountToRoomBill(roomId, amountToAdd);
+    }
+
+    public int getRoomBillAmount(String roomId) {
+        return bills.getRoomBillAmount(roomId);
+    }
+    
+    public boolean payAmountToRoomBill(String roomId, int amountToAdd) {
+        return bills.payAmountToRoomBill(roomId, amountToAdd);
+    }
+   
+
+    // Otros métodos de la clase EmployerProcess
+
+    // ...
 
 }
